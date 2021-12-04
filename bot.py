e72a20c3c09f18d9232e889b1e9f5a7761ca63bf
ymkeel <kesselyanniel@gmail.com>
2021-12-04 19:57:29 +0000

    Fixed error in start_webhook method

--- bot.py
+++ bot.py
@@ -57,7 +57,7 @@ def main():
     dp.add_error_handler(error)
 
     # Start the Bot
-    updater.start_webhook(listen='0.0.0.0', port=PORT url_path=TOKEN)
+    updater.start_webhook(listen='0.0.0.0', port=PORT, url_path=TOKEN)
     updater.bot.setWebhook('https://ykb-bot.herokuapp.com/' + TOKEN)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
